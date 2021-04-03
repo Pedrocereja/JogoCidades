8446b5746fff79e0cdf79722f1b2abc1d02463f6
Pedrocereja <pedro_cardoso_c@hotmail.com>
2021-04-03 12:30:44 -0300

    Advanced Dikstra

--- handlers/menorCaminho.lua
+++ handlers/menorCaminho.lua
@@ -1,11 +1,11 @@
-local nvst = tortas -- nodos não visitados
+local nvst = table.shallow_copy(tortas) -- nodos não visitados
 local mpath = {} -- tabela com nodo|peso|ult. visitado
 
 function menorCaminho(A, B)--, tipo)
 	-- Encontra o menor caminho entre duas tortas fornecidas
 
 	--local tipo = tipo or 1
-	local  adj = {} --gconstrói a tabela de nodos adjacentes ao atual
+	local  adj = {} --constrói a tabela de nodos adjacentes ao atual
 	for i,v in ipairs(caminhos) do
 		if v.T1 == A then
 			table.insert(adj, v.T2)
@@ -17,4 +17,12 @@ function menorCaminho(A, B)--, tipo)
 	for i,v in ipairs(adj) do
 		if v.vst
 	end
+end
+
+local function table.shallow_copy(t)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
 end
