9fa13878c585c7e08b3fd8b240f15e3cb65005e4
Pedrocereja <pedro_cardoso_c@hotmail.com>
2021-04-10 09:36:33 -0300

    Refactoring

--- handlers/Dijkstra.lua
+++ handlers/Dijkstra.lua
@@ -19,57 +19,6 @@ local function getDistance(Torta1, Torta2)
 	dist = math.sqrt(dist)
 return dist end
 
-local function setInitialValues(self)
-	for i,v in ipairs(self.nodos) do
-		self.distances[v] = math.huge
-		self.lastVisited[v] = nil
-		table.insert(self.unvisited, v)
-		v.iunvisited = #self.unvisited
-	end
-	self.distances[self.origem] = 0
-end
-
-local function getShorterNode(self)
-	local closerNode = self.unvisited[#self.unvisited]
-	for i,nodo in pairs(self.unvisited) do
-		if self.distances[nodo]<self.distances[closerNode] then
-			closerNode = nodo
-		end
-	end
-return closerNode end
-
-local function removeFromUnvisited(self, nodo)
-	table.remove(self.unvisited, nodo.iunvisited)
-	for i=nodo.iunvisited,#self.unvisited do
-		self.unvisited[i].iunvisited=self.unvisited[i].iunvisited-1
-	end
-end
-
-local function vizinhos(self, nodo)
-	local  adjacentes = {}
-	for i,aresta in ipairs(self.arestas) do
-		if aresta.inicio == nodo then
-			table.insert(adjacentes, aresta.fim)
-		elseif aresta.fim == nodo then
-			table.insert(adjacentes, aresta.inicio)
-		end 
-	end
-return adjacentes end
-
-local function setLastVisited(self)
-	while #self.unvisited>0 do
-		local actualNode = getShorterNode(self)
-		removeFromUnvisited(self, actualNode)
-		for i, vizinho in pairs(vizinhos(self, actualNode)) do
-			local distVizinho = self.distances[actualNode] + getDistance(actualNode, vizinho)
-			if distVizinho < self.distances[vizinho] then
-				self.lastVisited[vizinho] = actualNode
-				self.distances[vizinho] = distVizinho
-			end
-		end
-	end
-end
-
 --Dijkstra
 Dijkstra = Object:extend()
 
@@ -88,8 +37,8 @@ function Dijkstra:update(nodos, arestas)
 end
 
 function Dijkstra:menorCaminho(alvo)--, tipo)
-	setInitialValues(self)
-	setLastVisited(self)
+	Dijkstra:setInitialValues()
+	Dijkistra:setLastVisited()
 	local caminho = {}
 	local alvo = alvo
 	if self.lastVisited[alvo]~=nil and alvo~=self.origem then
@@ -100,3 +49,54 @@ function Dijkstra:menorCaminho(alvo)--, tipo)
 	end
 	caminho = inverterOrdem(caminho)
 return caminho end
+
+function Dijkstra:setInitialValues()
+	for i,v in ipairs(self.nodos) do
+		self.distances[v] = math.huge
+		self.lastVisited[v] = nil
+		table.insert(self.unvisited, v)
+		v.iunvisited = #self.unvisited
+	end
+	self.distances[self.origem] = 0
+end
+
+function Dijkistra:setLastVisited()
+	while #self.unvisited>0 do
+		local actualNode = Dijkistra:getShorterNode()
+		Dijkistra:removeFromUnvisited(actualNode)
+		for i, vizinho in pairs(Dijkistra:vizinhos(actualNode)) do
+			local distVizinho = self.distances[actualNode] + getDistance(actualNode, vizinho)
+			if distVizinho < self.distances[vizinho] then
+				self.lastVisited[vizinho] = actualNode
+				self.distances[vizinho] = distVizinho
+			end
+		end
+	end
+end
+
+function Dijkistra:getShorterNode()
+	local closerNode = self.unvisited[#self.unvisited]
+	for i,nodo in pairs(self.unvisited) do
+		if self.distances[nodo]<self.distances[closerNode] then
+			closerNode = nodo
+		end
+	end
+return closerNode end
+
+function Dijkistra:removeFromUnvisited(nodo)
+	table.remove(self.unvisited, nodo.iunvisited)
+	for i=nodo.iunvisited,#self.unvisited do
+		self.unvisited[i].iunvisited=self.unvisited[i].iunvisited-1
+	end
+end
+
+function Dijkistra:vizinhos(nodo)
+	local  adjacentes = {}
+	for i,aresta in ipairs(self.arestas) do
+		if aresta.inicio == nodo then
+			table.insert(adjacentes, aresta.fim)
+		elseif aresta.fim == nodo then
+			table.insert(adjacentes, aresta.inicio)
+		end 
+	end
+return adjacentes end
