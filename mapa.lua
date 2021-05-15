f51c60be2a1dce017388b54fe206a1ff0e7d0d4e
Pedrocereja <pedro_cardoso_c@hotmail.com>
2021-05-15 12:28:59 -0300

    Criados recursos como objetos

--- mapa.lua
+++ mapa.lua
@@ -1,35 +1,6 @@
-local tileset = love.graphics.newImage( "images/cartographypack/Spritesheet/spritesheet_default.png" )
 local bgImage = love.graphics.newImage( "images/cartographypack/Textures/parchmentAncient.png" )
-local tilesetW, tilesetH = tileset:getWidth(), tileset:getHeight()
 local tileW, tileH = 64,64
 
-local tiles = {}
-tiles[0] = love.graphics.newQuad(192,  320, tileW, tileH, tilesetW, tilesetH) --montanha
-tiles[1] = love.graphics.newQuad(384,  128, tileW, tileH, tilesetW, tilesetH) --terra
-tiles[2] = love.graphics.newQuad(0,  576, tileW, tileH, tilesetW, tilesetH)  --arvore
-tiles[3] = love.graphics.newQuad(128,  640, tileW, tileH, tilesetW, tilesetH)  --agua
-
-local map={
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 0, 2, 2, 2, 0, 3, 0, 3, 0, 1, 1, 1, 0},
-   { 0, 1, 0, 0, 2, 0, 2, 0, 3, 0, 3, 0, 1, 0, 0, 0},
-   { 0, 1, 1, 0, 2, 2, 2, 0, 0, 3, 0, 0, 1, 1, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
-   { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   { 0, 2, 2, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 2, 0, 0},
-   { 0, 2, 0, 0, 0, 3, 0, 3, 0, 1, 0, 1, 0, 2, 0, 0},
-   { 0, 2, 0, 0, 0, 3, 0, 3, 0, 1, 0, 1, 0, 2, 0, 0},
-   { 0, 2, 2, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 2, 2, 2},
-   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-}
-
-local mapW = #map[1]
-local mapH = #map
 local mapX = 0
 local mapY = 0
 local mapDisplayWidth = 25
@@ -37,36 +8,22 @@ local mapDisplayHeight = 19
 
 local numberOfBufferedTiles = 4
 
-function drawBG()
-   love.graphics.draw(
-              bgImage,
-              0,
-              0)
+local function drawBG()
+   love.graphics.draw(bgImage, 0, 0)
+end
+
+local function drawResources()
+   --to do
+end
+
+local function drawStructures()
+   --to do
 end
 
 function drawMap()
    drawBG()
-
-   local firstTileX = math.floor(mapX / tileW)
-   local firstTileY = math.floor(mapY / tileH)
-   local mapMaxYView = mapDisplayHeight + numberOfBufferedTiles
-   local mapMaxXView = mapDisplayWidth + numberOfBufferedTiles
-   for y=1, mapMaxYView do
-      for x=1, mapMaxXView do
-         if y+firstTileY >= 1 and y+firstTileY <= mapH
-            and x+firstTileX >= 1 and x+firstTileX <= mapW
-         then
-            local tileInView_Index = tiles[ map[y + firstTileY][x + firstTileX] ]
-            local tileInView_X = ( (firstTileX + x - 1) * tileW)
-            local tileInView_Y = ( (firstTileY + y - 1) * tileH)
-            love.graphics.draw(
-               tileset,
-               tileInView_Index,
-               tileInView_X,
-               tileInView_Y)
-         end
-      end
-   end
+   drawResources()
+   drawStructures()
 end
 
 function updateMapToCameraPosition()
@@ -75,10 +32,3 @@ function updateMapToCameraPosition()
    mapDisplayWidth = math.ceil(camera.w/camera.scale)/tileW
    mapDisplayHeight = math.ceil(camera.h/camera.scale)/tileH
 end
-
-function toMapCoordinates(x, y)
-   local tileX, tileY = math.floor(x/tileW) + 1, math.floor(y/tileH) + 1
-   if (tileX > mapW) or (tileY > mapH) then
-      return nil, nil
-   end
-return tileX, tileY end
---
+++ resources.lua
@@ -0,0 +1,43 @@
+local tileset = love.graphics.newImage( "images/cartographypack/Spritesheet/spritesheet_default.png" )
+local tilesetW, tilesetH = tileset:getWidth(), tileset:getHeight()
+local tileW, tileH = 64,64
+
+Recurso = Object:extend()
+
+function Recurso:new(quad, x, y)
+    local x, y = x, y
+    local quad
+    local state = "Full" --"Full", "Depleted"
+    local quantity = math.random(50, 200)
+end
+
+function Recurso:draw()
+    if self.state == "Depleted" then
+       return
+    elseif self.state == "Full" then
+        love.graphics.draw(tileset, self.quad, self.x, self.y)
+    end
+end
+
+
+--TODO alocar a criação de um tile em uma só função de criação?
+local tile = {}
+tile["montanha"] = love.graphics.newQuad(192,  320, tileW, tileH, tilesetW, tilesetH)
+tile["arvore"] = love.graphics.newQuad(0,  576, tileW, tileH, tilesetW, tilesetH)
+tile["agua"] = love.graphics.newQuad(128,  640, tileW, tileH, tilesetW, tilesetH)
+
+local function produceResourceType(quad, x, y)
+    local newResource = Recurso(quad, x, y)
+return newResource end
+
+function newForest(x, y)
+    forest = produceResourceType(tile["arvore"], x, y)
+return forest end
+
+function newMountain(x, y)
+    mountain = produceResourceType(tile["montanha"], x, y)
+return mountain end
+
+function newWater(x, y)
+    water = produceResourceType(tile["agua"], x, y)
+return water end
